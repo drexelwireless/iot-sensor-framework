dae9b57162fe45ec9d6638f2a3b2beae497bb7a5
Enioluwa Segun <eos24@drexel.edu>
2019-08-21 13:25:26 -0400

    Fix adding to database in xarray

--- interrogator/client_xarray.sh
+++ interrogator/client_xarray.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 client.py -i 10.248.101.225:80 -p abc123 -g xarray -d -l 0.75 -t 4
+python3 client.py -i 10.248.101.225:80 -p abc123 -g xarray -d -l 0.75 -t 4 -u admin -w kapilrocks
