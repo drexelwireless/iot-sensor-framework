05ee430c2dd7641931b7f5b66bb6c4012971eeb0
William Mongan <billmongan@gmail.com>
2019-11-15 12:27:32 -0500

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-if [[ -z `which python3` ]]
-then
-	alias python3="python"
-	alias pip3="pip"
-fi
-
 sudo apt-get update
 
 sudo apt-get -y install python3.6
 sudo apt-get -y install python-pip
 sudo apt-get -y install python3-pip
-sudo python3 -m pip uninstall pip && sudo apt install -y python3-pip --reinstall
 sudo apt-get -y install python-dev
 sudo apt-get -y install python3-dev
 pip3 install --upgrade pip
@@ -83,4 +76,4 @@ sudo apt-get install libgsl0ldbl
 git clone https://github.com/ajmendez/PyMix.git
 touch PyMix/README.rst
 sed 's/from distutils.core import setup, Extension,DistutilsExecError/#from distutils.core import setup, Extension,DistutilsExecError\nfrom distutils.core import setup, Extension' PyMix/setup.py
-python PyMix/setup.py install
+python3 PyMix/setup.py install
