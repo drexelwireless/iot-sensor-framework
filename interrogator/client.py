a7e11460a942643b27695c9c520057e55f742957
William Mongan <billmongan@gmail.com>
2020-11-23 17:08:48 -0500

    r420 reconfigurable client

--- interrogator/client.py
+++ interrogator/client.py
@@ -1,12 +1,13 @@
 from impinj import *
 from impinj_r420 import *
 from impinj_xarray_itemsense_localization import *
+from impinj_r420_reconfigurable import *
 import getopt
 import sys
 import os
 import time
 
-def usage(ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, recipe, facility):
+def usage(ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, recipe, facility, controllerip, controllerport):
     print('%s [<options>]' % sys.argv[0])
     print('where <options> are:\n' \
         '\t-h - show this help message\n' \
@@ -16,14 +17,16 @@ def usage(ip_address, db_host, db_password, cert_path, do_debug, device, antenna
         '\t-c <certfile> - path to the certificate file for verifying SSL certificate or NONE to bypass: default %s\n' \
         '\t-a <antenna number> - antenna number to interrogate (can specify more than once), if supported by the interrogator: default %s\n' \
         '\t-l <time in seconds> - length of time to sleep the dispatcher in between transmissions of data to the server, to allow new messages to queue up for efficiency: default %s\n' \
-        '\t-g <device> - device to use as the interrogator (impinj, r420, xarray): default %s\n' \
+        '\t-g <device> - device to use as the interrogator (impinj, r420, r420reconfigurable, xarray): default %s\n' \
         '\t-t <pop> - sets tag population (4 good for 1 tag, 16 good for 2 tags): default %s\n' \
         '\t-u <api-username> - provides a username for API services such as the Impinj xArray\n' \
         '\t-w <api-password> - provides a password for API services such as the Impinj xArray\n' \
         '\t-r <xarray-recipe> - provides a Recipe name for ItemSense if using the Impinj xArray: default %s\n' \
         '\t-f <xarray-facility> - provides a Facility name for ItemSense if using the Impinj xArray: default %s\n' \
+        '\t-x <controller-ip> - IP address of an external controller i.e. for a reconfigurable antenna: default %s\n' \
+        '\t-y <controller-port> - port number of an external controller i.e. for a reconfigurable antenna: default %s\n' \
         '\t-d - Enable debugging: default %s\n' % (ip_address, db_host, db_password,
-                                                   cert_path, antennas, dispatchsleep, device, tagpop, recipe, facility, do_debug))
+                                                   cert_path, antennas, dispatchsleep, device, tagpop, recipe, facility, controllerip, controllerport, do_debug))
     sys.exit(1)
 
 
@@ -43,14 +46,16 @@ def getopts():
     apipassword = "NONE"
     recipe = "IMPINJ_Fast_Location"
     facility = "MESS"
+    controllerip = "localhost"
+    controllerport = 8080
 
     # Check command line
-    optlist, list = getopt.getopt(sys.argv[1:], 'hi:o:p:dc:g:a:n:l:t:u:w:r:f:')
+    optlist, list = getopt.getopt(sys.argv[1:], 'hi:o:p:dc:g:a:n:l:t:u:w:r:f:x:y:')
 
     for opt in optlist:
         if opt[0] == '-h':
             usage(ip_address, db_host, db_password, cert_path, do_debug,
-                  device, antennas, dispatchsleep, tagpop, recipe, facility)
+                  device, antennas, dispatchsleep, tagpop, recipe, facility, controllerip, controllerport)
         if opt[0] == '-i':
             ip_address = opt[1]
         if opt[0] == '-o':
@@ -76,14 +81,18 @@ def getopts():
         if opt[0] == '-r':
             recipe = opt[1]
         if opt[0] == '-f':
-            facility = opt[1]            
+            facility = opt[1]      
+        if opt[0] == '-x':
+            controllerip = opt[1]
+        if opt[0] == '-y':
+            controllerport = int(opt[1])
 
     if len(userantennas) > 0:
         antennas = userantennas
 
     #print ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep
 
-    return ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, apiusername, apipassword, recipe, facility
+    return ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, apiusername, apipassword, recipe, facility, controllerip, controllerport
 
 
 def print_stats(rfid):
@@ -112,7 +121,7 @@ def prog_quit(rfid, QUITFILE='quit'):
             os._exit(0)
 
 if __name__ == "__main__":
-    ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, apiusername, apipassword, recipe, facility = getopts()
+    ip_address, db_host, db_password, cert_path, do_debug, device, antennas, dispatchsleep, tagpop, apiusername, apipassword, recipe, facility, controllerip, controllerport = getopts()
 
     if device.lower() == "impinj":
         rfid = Impinj(ip_address, db_host, db_password, cert_path,
@@ -126,6 +135,13 @@ if __name__ == "__main__":
         t2 = threading.Thread(target=prog_quit, args=(rfid,))
         t2.start()
         rfid.start()
+    elif device.lower() == "r420reconfigurable":
+        rfid = ImpinjR420Reconfigurable(ip_address, db_host, db_password, cert_path, do_debug,
+                          _dispatchsleep=dispatchsleep, _antennas=antennas, _tagpop=tagpop, 
+                          _antennaclientip=controlleripd, _antennaclientport=controllerport)
+        t2 = threading.Thread(target=prog_quit, args=(rfid,))
+        t2.start()
+        rfid.start()        
     elif device.lower() == "xarray":
         rfid = ImpinjXArray(ip_address, db_host, db_password, cert_path,
                             do_debug, _dispatchsleep=dispatchsleep,
