22feca5246d1cb725522d53096a2979c2dc8ed1b
William Mongan <billmongan@gmail.com>
2021-10-26 15:58:23 -0400

    R700 updates from Kevin, Ian, and Noah

--- interrogator/impinj_r700.py
+++ interrogator/impinj_r700.py
@@ -9,6 +9,11 @@ import os
 import queue
 from time import sleep
 import collections
+import random
+import socket
+import math
+import numpy as np
+from datetime import datetime
 
 #added
 import requests
@@ -17,6 +22,10 @@ try:
     from urlparse import urljoin
 except ImportError:
     from urllib.parse import urljoin
+
+#import localization noah class
+from localize_noah import localizer
+
 
 ''' 
 -------------------------------------------------------------------------------------------------------    
@@ -52,24 +61,33 @@ except ImportError:
     - Changed the names of relevant and present parameters in handler_thread()
 '''
 
-class ImpinjR700(Interrogator):
+class ImpinjR700Reconfigurable(Interrogator):
     def __init__(self, _ip_address, _db_host, _db_password, _cert_path, _debug, _dispatchsleep=0, _antennas=[], _tagpop=4):
         Interrogator.__init__(self, _db_host, _db_password,
                               _cert_path, _debug, _dispatchsleep)
         self.exiting = False
         self.ip_address = _ip_address
+        self.hostname = 'http://{0}'.format(self.ip_address)
         if len(_antennas) > 0:
             self.antennas = _antennas
         else:
             self.antennas = [1, 2, 3, 4]
         self.tagpop = _tagpop
 
+        self.txPower = 3000
+        
+        self.fac = None
+
         if self.cert_path != 'NONE':
             self.http_obj = Http(ca_certs=self.cert_path)
         else:
             self.http_obj = Http(disable_ssl_certificate_validation=True)
-
+        
         self.out('Initializing R700 interrogator client')
+
+        #temporarily use these to calculate doppler
+        self.last_time={}
+        self.last_phase={}
 
     def out(self, x):
         if self.debug:
@@ -77,17 +95,17 @@ class ImpinjR700(Interrogator):
 
     def start_server(self):
         # Handle Ctrl+C interrupt (might not be needed)
-        signal.signal(signal.SIGINT, signal_handler)
+        signal.signal(signal.SIGINT, self.signal_handler)
         
         self.out('Starting Impinj R700 interrogator client')
         
         reader=self.ip_address 
-        toStart='default' #choose a mode to run in
-        hostname = 'http://{0}'.format(reader)
+        toStart='dwsl' #choose a mode to run in (i.e., default)
+        #hostname = 'http://{0}'.format(reader)
         
         #try to connect to r700
         try:
-            requests.get(urljoin(hostname, '/api/v1/status')).raise_for_status()
+            requests.get(urljoin(self.hostname, '/api/v1/status')).raise_for_status()
         except (requests.ConnectionError, requests.exceptions.HTTPError):
             self.out('Error : Unable to connect to the Impinj Reader API on "{0}"'.format(hostname))
             if len(hostname.split(':')) == 2:
@@ -95,9 +113,24 @@ class ImpinjR700(Interrogator):
                 self.out('        ex.  --reader <your-reader-hostname>:<api-port>')
             sys.exit(1)
         
-        requests.post(urljoin(hostname, 'api/v1/profiles/stop')) # Stop the active preset
-        requests.post(urljoin(hostname, 'api/v1/profiles/inventory/presets/{0}/start'.format(toStart))) # Start the default preset
-        for event in requests.get(urljoin(hostname, 'api/v1/data/stream'), stream=True).iter_lines(): # Connect to the event stream
+        requests.post(urljoin(self.hostname, 'api/v1/profiles/stop')) # Stop the active preset
+        # Do phase
+        #defaultConfig = {"antennaConfigs" : [{"antennaPort": 1, "transmitPowerCdbm": 3000, "inventorySession": 2, "inventorySearchMode": "dual-target", "estimatedTagPopulation": 32, "rfMode": 1110 } ] }
+        
+        # For after firmware update
+        defaultConfig = {"eventConfig": {"common": {"hostname": "disabled"},"tagInventory": {"tagReporting": {"reportingIntervalSeconds": 0,"antennaIdentifier": "antennaPort","tagIdentifier": "epc"},"epc": "enabled","epcHex": "enabled","tid":"enabled","tidHex": "enabled","antennaPort": "enabled","transmitPowerCdbm": "enabled","peakRssiCdbm": "enabled","frequency": "enabled","pc": "enabled","lastSeenTime": "enabled","phaseAngle": "enabled"}}, "antennaConfigs" : [{"antennaPort": 1, "transmitPowerCdbm": self.txPower, "inventorySession": 2, "inventorySearchMode": "dual-target", "estimatedTagPopulation": 32, "rfMode": 1110 } ] }
+
+        #resp = requests.get(urljoin(self.hostname, 'api/v1/profiles/inventory/presets/{0}'.format("default")))
+        #print(resp)
+        #print(resp.content)
+
+        resp = requests.put(urljoin(self.hostname, 'api/v1/profiles/inventory/presets/{0}'.format(toStart)), data=json.dumps(defaultConfig).encode('utf-8'), headers={"Content-Type": "application/json"})
+        # print(json.dumps(defaultConfig).encode('utf-8'))
+        #print(resp)
+        #print(resp.content)
+
+        requests.post(urljoin(self.hostname, 'api/v1/profiles/inventory/presets/{0}/start'.format(toStart))) # Start the default preset
+        for event in requests.get(urljoin(self.hostname, 'api/v1/data/stream'), stream=True).iter_lines(): # Connect to the event stream
             #print for debug
             self.out(event)
             self.handle_event( event )
@@ -157,7 +190,9 @@ class ImpinjR700(Interrogator):
 
             input_msg = self.handler_dequeue.popleft()
             #added .json() to be able to parse input_msg like a dict
-            input_msgs.append( input_msg.json() )
+            input_msg = input_msg.decode("utf-8")    
+            input_msg = json.loads(input_msg)  
+            input_msgs.append( input_msg )
 
             # Subtags like <EPC> may or may not be present
             # <RO_ACCESS_REPORT>
@@ -184,11 +219,91 @@ class ImpinjR700(Interrogator):
             for msg in input_msgs:
                 self.out(msg)
 
-                tags = msg.msgdict['RO_ACCESS_REPORT']['TagReportData']
+                #tags = msg.msgdict['RO_ACCESS_REPORT']['TagReportData']
 
-                self.out(tags)
+                #self.out(tags)
+                first_seen_timestamp = msg['timestamp'] 
+                epc96 = msg['tagInventoryEvent']['epcHex'] 
+                antenna = msg['tagInventoryEvent']['antennaPort'] 
+                rssi = msg['tagInventoryEvent']['peakRssiCdbm'] / 100
+                transmit_power = msg['tagInventoryEvent']['transmitPowerCdbm'] 
+                channelindex = int(((msg['tagInventoryEvent']['frequency'] - 902750) / 500) + 1)
+                
+                # added phase and doppler reporting
+                if 'phaseAngle' in msg['tagInventoryEvent']:
+                    phase = msg['tagInventoryEvent']['phaseAngle'] 
+                else:
+                    phase = 0
 
-                for tag in tags:
+                if 'doppler' in msg['tagInventoryEvent']:
+                    doppler = msg['tagInventoryEvent']['doppler']
+                else:
+                    doppler = 0
+                
+                self.count = self.count + 1
+                
+                # converted first_seen_timestamp to milliseconds because strptime is incompatible with date format
+                first_seen_timestamp = first_seen_timestamp.replace('Z', '')
+                first_seen_timestamp_nanos = first_seen_timestamp[-3:]
+                first_seen_timestamp_nanos = int(first_seen_timestamp_nanos)
+                first_seen_timestamp = first_seen_timestamp[:-3] # remove nanoseconds for strptime
+                dt_first_seen_timestamp = datetime.strptime(first_seen_timestamp.replace('Z', ''), '%Y-%m-%dT%H:%M:%S.%f')
+                first_seen_timestamp = int(dt_first_seen_timestamp.timestamp() * 1000)
+               # first_seen_timestamp = first_seen_timestamp + first_seen_timestamp_nanos
+
+                # if this is the "first" firstseentimestamp, note that so the other times will be relative to that
+                if self.start_timestamp == 0:
+                    self.start_timestamp = first_seen_timestamp
+
+                self.latest_timestamp = first_seen_timestamp
+               
+
+                lastseentimestamp = first_seen_timestamp
+                
+                #TODO: calculate doppler here
+                if not epc96 in self.last_phase.keys():
+                    doppler=0
+                else:
+                    if lastseentimestamp==self.last_time[epc96]:
+                        doppler=0
+                        self.out('doppler: no delta time')
+                    else:
+                        doppler=(1/(4*math.pi))*(phase-self.last_phase[epc96])/(lastseentimestamp-self.last_time[epc96])
+                
+                self.last_phase[epc96]=phase
+                self.last_time[epc96]=lastseentimestamp
+
+                # doppler = 0
+                # phase = 0
+                rospecid = 1
+                tagseencount = 1
+                accessspecid = 1
+                inventoryparameterspecid = 1                
+                
+                self.out('phase was: {}'.format(phase))
+                phase = phase*math.pi/180
+                self.out('phase is: {}'.format(phase))
+
+                freeform = {}
+                freeform['rssi'] = rssi
+                freeform['epc96'] = epc96
+                freeform['doppler'] = doppler
+                freeform['phase'] = phase
+                freeform['antenna'] = antenna
+                freeform['rospecid'] = rospecid
+                freeform['channelindex'] = channelindex
+                freeform['tagseencount'] = tagseencount
+                freeform['accessspecid'] = accessspecid
+                freeform['inventoryparameterspecid'] = inventoryparameterspecid
+                freeform['lastseentimestamp'] = lastseentimestamp
+            
+                freeformjson = json.dumps(freeform)
+
+                # call self.insert_tag to insert into database
+                self.insert_tag(first_seen_timestamp, freeformjson, self.start_timestamp)
+                                
+
+                """for tag in tags:
                     #changes
                     '''
                     FirstSeenTimestampUTC -> timestamp
@@ -270,17 +385,24 @@ class ImpinjR700(Interrogator):
                     freeform['inventoryparameterspecid'] = inventoryparameterspecid
                     freeform['lastseentimestamp'] = lastseentimestamp
                     
+                    if self.reconfigurableantennastate != -1: # if we're using reconfigurable antennas, include the state
+                        freeform['antennastate'] = self.reconfigurableantennastate
+                    
                     freeformjson = json.dumps(freeform)
 
                     # call self.insert_tag to insert into database
                     self.insert_tag(first_seen_timestamp, freeformjson, self.start_timestamp)
-
+"""
     def close_server(self):
         #if getting rid of fac clients, need to change this
         self.exiting = True
-        self.fac.politeShutdown()
-        reactor.stop()
+        requests.post(urljoin(self.hostname, 'api/v1/profiles/stop')) # Stop the active preset
+        try:
+            reactor.stop()
+        except:
+            pass
         if not (self.fac is None):
+            self.fac.politeShutdown()
             if not (self.fac.proto is None):
                 self.fac.proto.exiting = True
 
@@ -300,8 +422,11 @@ class ImpinjR700(Interrogator):
         phase = freeform['phase']
         channelindex = freeform['channelindex']
         doppler = freeform['doppler']
-        self.out("Adding tag %s with RSSI %s and timestamp %s and ID %s on antenna %s with Phase %s and Doppler %s and Channel %s" % (
-            str(self.count), str(peak_rssi), str(first_seen_timestamp), str(epc), str(antenna), str(phase), str(doppler), str(channelindex)))
+        antennastate = '-1'
+        if 'antennastate' in freeform:
+            antennastate = freeform['antennastate']
+        self.out("Adding tag %s with RSSI %s and timestamp %s and ID %s on antenna %s with Phase %s and Doppler %s and Channel %s and antenna state %s" % (
+            str(self.count), str(peak_rssi), str(first_seen_timestamp), str(epc), str(antenna), str(phase), str(doppler), str(channelindex), str(antennastate)))
 
         input_dict = dict()
         input_dict['data'] = dict()
@@ -312,9 +437,12 @@ class ImpinjR700(Interrogator):
         input_dict['data']['interrogator_time'] = first_seen_timestamp
 
         self.tag_dicts_queue.put(input_dict)  # read by the consumer
+        
+        self.antennathreadhistory.put(input_dict)
 
-def signal_handler(signal, frame):
-    sys.exit(0)
+    def signal_handler(self, signal, frame):
+        self.close_server()
+        sys.exit(0)
 
 
 
