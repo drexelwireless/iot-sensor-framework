7e557478990622d95bca62b98bd9e059ac5620ff
William Mongan <billmongan@gmail.com>
2022-02-02 17:27:44 -0500

    flattening dict

--- database/database_variot.py
+++ database/database_variot.py
@@ -12,17 +12,12 @@ import pycurl
 import json
 import io
 import requests
+import time
 
-# Set up MAC address on server when ready, and use mac address as device ID; possibly token in the future
 class VarIOTDatabase(Database):
-    def __init__(self, crypto, db_path='https://variot.ece.drexel.edu', token=None, device='0000000000', dispatchsleep=0, username='', password=''):
+    def __init__(self, crypto, db_path='https://variot.ece.drexel.edu', token='e2tmxfzcJG6JfPuyzTOT', device='6592aa50-579f-11ec-b72a-b3a72eee71ca', dispatchsleep=0):
         Database.__init__(self, crypto, db_path=db_path)
-        self.username = username
-        self.password = password
-        if not (token is None):
-            self.token = token
-        else:
-            self.token = self.getToken()
+        self.token = token
         self.insertion_queue = queue.Queue()
         self.dispatcher_thread = threading.Thread(
             target=self.dispatcher, args=())
@@ -30,31 +25,12 @@ class VarIOTDatabase(Database):
         self.dispatchsleep = dispatchsleep
         self.dev = device
 
-    # https://thingsboard.io/docs/pe/user-guide/attributes/
-    def postDeviceAttribute(self, attrname, attrvalue):
-        payload = {attrname: attrvalue}
-        headers = {"Authorization": "Bearer " + self.token}
-        URL = self.db_path + '/api/plugins/telemetry/DEVICE/' + self.dev + '/SERVER_SCOPE'
-        r = requests.post(url = URL, json = payload, headers = headers, verify=False)    
-        resp = r.text
-        # print(resp)
-        return resp     
-        
-    def getToken(self):
-        payload = {'username': self.username, 'password': self.password}
-        URL = self.db_path + '/api/auth/login'
-        r = requests.post(url = URL, json = payload, verify=False)    
-        resp = r.text
-        # print(resp)
-        respjson = json.loads(resp)
-        return respjson['token']
-    
     def variot_dispatch(self, recordsdictlist):
         # Remove password since there is no application level encryption to VarIOT
         for record in recordsdictlist:
-            del record['db_pw']
-            
-        data = json.dumps(recordsdictlist)
+            del record['values']['db_pw']
+            #record['ts']=int(time.time())
+            #print(int(time.time()))
 
         # TODO POST TO VARIOT HERE
         # data is a json array of json records
@@ -63,17 +39,12 @@ class VarIOTDatabase(Database):
         # Viewable at http://10.248.101.200:5000/messages/5e4af7041c9d440000f0cd38
         # Post to https?
         # Which side encrypts?  Right now, eliminating this side encryption for VarIOT (hence removal of password from the body and use of crypto from this module
-        # Documentation of REST API: https://thingsboard.io/docs/reference/rest-api/
-        # payload = {'ts': record['interrogatortime'], 'values': json.dumps(data)}
-        #headers = {"Authorization": "Bearer " + self.token}
-        ##URL = self.db_path + '/api/v2/hubs/message/xarray?address=' + self.dev
-        #URL = self.db_path + '/api/v1/' + self.token + '/telemetry'
-        #r = requests.post(url = URL, json = payload, headers = headers, verify=False)
-        #print(r.status_code)
-        #print(r.text)        
-        
-        self.postDeviceAttribute('data', json.dumps(data))
-
+        for payload in recordsdictlist:
+        	URL = self.db_path + f'/api/v1/{self.token}/telemetry'
+        	r = requests.post(url = URL, json = payload, verify=False, headers={"Content-Type":"application/json"})
+        	print('Status code: '+str(r.status_code))
+        	print('Payload: '+str(payload))
+	
     # dispatch insertions from the queue so that the webserver can continue receiving requests
     # log each request to the Audit
     def dispatcher(self):
@@ -101,9 +72,19 @@ class VarIOTDatabase(Database):
     # just insert into a queue for the dispatcher to insert in the background
     def insert_row(self, relativetime, interrogatortime, freeform, db_pw=''):
         input_dict = dict()  # read by the consumer dispatcher
-        input_dict['relativetime'] = relativetime
+        #input_dict['ts'] = int(time.time()) #changed input_dict field to ts from relativetime
+        #input_dict['values']={}
+        #input_dict['values']['interrogatortime'] = interrogatortime
+        #input_dict['values']['relativetime'] = relativetime
         input_dict['interrogatortime'] = interrogatortime
-        input_dict['freeform'] = freeform
-        input_dict['db_pw'] = db_pw
+        input_dict['relativetime'] = relativetime
+        
+        for key in freeform:
+        	#input_dict['values'][key] = freeform[key]
+            input_dict[key] = freeform[key]
+            
+        input_dict['values']['db_pw'] = db_pw
 
         self.insertion_queue.put(input_dict)
+        #TODO: Add fetch_since method
+        
